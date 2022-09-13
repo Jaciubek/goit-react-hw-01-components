52fdd8ec53892a18d6a1e07c4d06b782746fffa0
Jaciubek <slawomir.jaciubek@gmail.com>
2022-09-13 15:56:26 +0200

    change className in Profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,44 +3,44 @@ import styles from './Profile.module.css';
 
 export const Profile = ({ username, tag, location, avatar, stats }) => {
     const {
-        card,
-        description,
-        avatarImg,
-        name,
-        usertag,
-        userlocation,
-        userstats,
-        statsItem,
-        label,
-        quantity,
+        userCard,
+        userDescription,
+        userAvatarImg,
+        userName,
+        userTag,
+        userLocation,
+        userStats,
+        userStatsItem,
+        userLabel,
+        userQuantity,
     } = styles;
     
     return (
         <section className="profile">
-            <div className={card}>
-                <div className={description}>
-                    <img src={avatar} alt="User avatar" className={avatarImg} />
-                    <p className={name}>{username}</p>
-                    <p className={usertag}>@{tag}</p>
-                    <p className={userlocation}>{location}</p>
+            <div className={userCard}>
+                <div className={userDescription}>
+                    <img src={avatar} alt="User avatar" className={userAvatarImg} />
+                    <p className={userName}>{username}</p>
+                    <p className={userTag}>@{tag}</p>
+                    <p className={userLocation}>{location}</p>
                 </div>
 
-                <ul className={userstats}>
-                    <li className={statsItem}>
-                        <span className={label}>Followers</span>
-                        <span className={quantity}>
+                <ul className={userStats}>
+                    <li className={userStatsItem}>
+                        <span className={userLabel}>Followers</span>
+                        <span className={userQuantity}>
                             {stats.followers.toLocaleString('en-EN')}
                         </span>
                     </li>
-                    <li className={statsItem}>
-                        <span className={label}>Views</span>
-                        <span className={quantity}>
+                    <li className={userStatsItem}>
+                        <span className={userLabel}>Views</span>
+                        <span className={userQuantity}>
                             {stats.views.toLocaleString('en-EN')}
                         </span>
                     </li>
-                    <li className={statsItem}>
-                        <span className={label}>Likes</span>
-                        <span className={quantity}>
+                    <li className={userStatsItem}>
+                        <span className={userLabel}>Likes</span>
+                        <span className={userQuantity}>
                             {stats.likes.toLocaleString('en-EN')}
                         </span>
                     </li>
